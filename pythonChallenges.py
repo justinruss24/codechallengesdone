f81c2aed09ae6ecbe33a8c14fa837e649f6ae9a2
Justin Russell <Justin@justins-macbook.lan>
2020-10-14 14:12:29 -0700

    adding coding challenges I've completed on CodeSignal

---
+++ pythonChallenges.py
@@ -0,0 +1,19 @@
+# Add Two Digits
+
+def addTwoDigits(n):
+    stringify = str(n)
+    return int(stringify[0]) + int(stringify[1])
+# Largest Number
+
+def largestNumber(n):
+    nines = "9" * n
+    return int(nines)
+
+
+# Candies
+
+# Seats in Theater
+
+# Max Multiple
+
+# Circle of Numbers
